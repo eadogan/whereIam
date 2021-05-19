4305e61487833940b8686b2bb88daa93967a6f5e
eadogan <e.ahmetdogan@gmail.com>
2021-05-19 20:54:28 +0100

    Refactor app.ts

---
+++ dist/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./src/app.ts ***!
+  \********************/
+
+console.log('deneme');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93aGVyZWlhbS8uL3NyYy9hcHAudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQWE7QUFDYiIsImZpbGUiOiJidW5kbGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJcInVzZSBzdHJpY3RcIjtcbmNvbnNvbGUubG9nKCdkZW5lbWUnKTtcbiJdLCJzb3VyY2VSb290IjoiIn0=
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+const form = document.querySelector('form')! ;
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+}
+
+form.addEventListener('submit', );
